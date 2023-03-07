67b67a9f1391d84068b880950287fd60126ffd21
Pham Minh Luan <minhluanpham1309@gmail.com>
2023-03-07 21:30:05 +0700

    Update FLightService.java

--- src/main/java/com/project/FlightJDBC/service/FLightService.java
+++ src/main/java/com/project/FlightJDBC/service/FLightService.java
@@ -1,22 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.project.FlightJDBC.service;
 
+//<editor-fold defaultstate="collapsed" desc="comment">
 import com.project.FlightJDBC.entity.Flight;
 import java.util.List;
+//</editor-fold>
 
 /**
  *
- * @author minhluan
+ * @author Pham Minh Luan <phamminhluan@fabercompany.co.jp>
  */
-public interface IFLightService {
+public interface FlightService {
+
     List<Flight> findAll();
+
     List<Flight> findByFromTo(String from, String to);
+
     Flight findById(Long id);
+
     Flight save(Flight flight);
+
     Flight update(Flight flight);
+
     void delete(Long id);
 }
